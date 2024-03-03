c24e2a5ced5ddd1b83386b690dab2d195a27b8ee
David Davó <david@ddavo.me>
2024-03-03 19:07:41 +0000

    Better paths to include other things in the filename

--- src/paths.py
+++ src/paths.py
@@ -4,8 +4,11 @@ from warnings import warn
 HYBRID_BASE_PATH = Path('./data/hybrid')
 PLN_BASE_PATH = Path('./data/pln')
 
-def _gen_fname(prefix, org_name, splits_freq, normalize, ext='csv') -> str:
-    return f'{prefix}-{org_name}-{splits_freq}{"-normalize" if normalize else ""}.{ext}'
+def _gen_fname(prefix, org_name, splits_freq, normalize, ext='csv', **kwargs) -> str:
+    other_args = "-".join([ f"{k}={v}" for k,v in kwargs.items() if v ])
+    
+    other_args = "-" + other_args if other_args else ""
+    return f'{prefix}-{org_name}-{splits_freq}{"-normalize" if normalize else ""}{other_args}.{ext}'
 
 def hybrid_best_hparams(org_name: str, splits_freq: str, normalize: bool, base: Path=HYBRID_BASE_PATH) -> Path:
     base.mkdir(exist_ok=True)
@@ -33,6 +36,8 @@ def pln_embeddings_cache(model, base: Path = PLN_BASE_PATH):
     
     return base / f'embeddings-{modelhash.hexdigest()}.pkl'
 
-def pln_mdf(org_name, splits_freq, normalize, base: Path=PLN_BASE_PATH):
+def pln_mdf(org_name, splits_freq, normalize, cutoff_date, *args, base: Path=PLN_BASE_PATH):
+    assert not args, "Unrecognized args"
+
     base.mkdir(exist_ok=True)
-    return base / _gen_fname('mdf', org_name, splits_freq, normalize, ext='pkl')
+    return base / _gen_fname('mdf', org_name, splits_freq, normalize, ext='pkl', cutoff_date=cutoff_date)
